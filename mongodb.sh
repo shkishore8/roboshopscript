689e4c5f9232f5db21a09ef790ba33bcbc79778a
kishore babu <shkishore8@gmail.com>
2025-11-18 10:25:56 +0530

    bondip config changed

--- mongodb.sh
+++ mongodb.sh
@@ -3,6 +3,6 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-org -y
 
-sed -i '/^bindIp s/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 
 systemctl restart mongod
