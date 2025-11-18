cc31d7783050ffd825fea74df42070ffe27cf4d0
kishore babu <shkishore8@gmail.com>
2025-11-18 11:53:53 +0530

    cataloue config file added

--- catalogue.sh
+++ catalogue.sh
@@ -5,8 +5,9 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
 useradd roboshop
-cp catalogue.service /etc/systemd/system/catalogue.service
 
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 mkdir /app
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
@@ -16,12 +17,15 @@ unzip /tmp/catalogue.zip
 cd /app
 npm install
 
+dnf install mongodb-mongosh -y
+mongosh --host 172.31.65.47 </app/db/master-data.js
+
+
 systemctl daemon-reload
 
 systemctl enable catalogue
 systemctl restart catalogue
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-dnf install mongodb-mongosh -y
-mongosh --host 172.31.65.47 </app/db/master-data.js
+
+
