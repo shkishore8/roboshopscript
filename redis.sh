6bff0129eb22fa997c2f1191d8a8394ba91eef54
kishore babu <shkishore8@gmail.com>
2025-11-18 13:07:04 +0530

    install and config cart server

--- redis.sh
+++ redis.sh
@@ -3,7 +3,7 @@ dnf module enable redis:7 -y
 
 dnf install redis -y
 
-sed -i 's/127.0.0.1/0.0.0.0/h' /etc/redis/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 
 systemctl enable redis
 systemctl rstart redis
